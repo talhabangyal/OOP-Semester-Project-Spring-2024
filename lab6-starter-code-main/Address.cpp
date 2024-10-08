4b32140965b76b706b81862f3d1b37332a3c23a7
Talha Abdullah Bangyal <talhaabdullahbangyal@gmail.com>
2024-10-08 13:44:04 +0500

    Create Address.cpp

---
+++ lab6-starter-code-main/Address.cpp
@@ -0,0 +1,43 @@
+#include "Address.h"
+
+
+/*
+*	Implement a constructor that initializes all the address parts
+*
+*/
+
+
+/*
+*	Implement getter/setters for all the member variables of Address. Also declare them in the Address.h header file
+*
+*/
+
+
+
+/*
+*	Implement the equals function that takes an Address object and checks if it is equal to
+*	current object refered by (this pointer)
+*	Two addresses are equal if all the attributes of addresses are equal
+*/
+bool Address::equals(const Address& address)
+{
+	/*
+	* Todo
+	*/
+	return false;
+}
+
+void Address::print_address()
+{
+	/*
+	*	Print address on one line as
+	*	House, Street, City, Country
+	*/
+}
+
+Address Address::copy_address()
+{
+	/*
+		return a copy of this
+	*/
+}
